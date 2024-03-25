5db70430ecb0981bdfb3a1798b6f3f39ed450295
yuvalgos <yuvalgos@gmail.com>
2024-03-25 11:24:49 +0200

    commit before moving to robotplanning in the planner class

--- scratch.py
+++ scratch.py
@@ -59,16 +59,16 @@ if __name__ == '__main__':
     # set robot position in the world:
     robot = world.robot(0)
 
-    collider = WorldCollider(world)
+    # collider = WorldCollider(world)
     # sim = Simulator(world)
-    cspace = RobotCSpace(robot, collider)
-    cspace.eps = 1e-2
+    # cspace = RobotCSpace(robot, collider)
+    # cspace.eps = 1e-3
     # cspace.setup()
-    MotionPlan.setOptions(type="rrt", bidirectional=True, shortcut=True,)
-    planner = MotionPlan(cspace)
+    # MotionPlan.setOptions(type="rrt", bidirectional=True, shortcut=True,)
 
     vis.add("world", world)
     vis.show()
+    time.sleep(1)
 
     start_c = robot.getConfig()
     # can set config then get ee
@@ -78,34 +78,40 @@ if __name__ == '__main__':
     # move in y towards the base:
     # goal_T = list(start_T)
     # goal_T[1][1] -= 0.4
-    goal_T = [[0, 0, -1, 0, 1, 0 ,1, 0, 0], [-0.19144999999599827, 0.4172500000009377, 0.97135]]
+    goal_T_1 = [[0, 0, -1, 0, 1, 0, 1, 0, 0], [-0.19144999999599827, 0.4172500000009377, 0.97135]]
+    # same as 1 but rotate around z by 90 degrees:
+    goal_T_2 = [[0, -1, 0, 0, 0, 1, -1, 0, 0], [-0.19144999999599827, 0.4172500000009377, 0.97135]]
 
 
-    goal_objective = ik.objective(body=ee_link, R=goal_T[0], t=goal_T[1])
+    goal_objective_1 = ik.objective(body=ee_link, R=goal_T_1[0], t=goal_T_1[1])
+    goal_objective_2 = ik.objective(body=ee_link, R=goal_T_2[0], t=goal_T_2[1])
 
-    mp = robotplanning.plan_to_cartesian_objective(world, robot, goal_objective)
+    goal_objective = [goal_objective_1, ]
 
-    print("goal_T: ", goal_T)
-    if not ik.solve_global(goal_objective):
-        print("no ik solution for goal")
-    goal_c = robot.getConfig()
-
-    robot.setConfig(start_c)
-    # look at noself collision in URDF. robot is coliding with itself at zero config
-
-    planner.setEndpoints(start_c, goal_c)
-
+    # TODO: move to this : plan options dict is argument, add also cspace planning option
+    # TODO: plan to multiple objectives (90 degrees rotation) and stop when one is reached, need to add constraints
+    # TODO:     Use OR constraints example from chatgpt below commented
     t = time.time()
+    mp = robotplanning.plan_to_cartesian_objective(world, robot, goal_objective,
+                                                   type="rrt*", bidirectional=True,)
+    mp.space.eps = 1e-2
     path = None
     while path is None:
         print("planning...")
-        planner.planMore(50)
-        path = planner.getPath()
-
+        mp.planMore(100)
+        # check if one of the goals is reached
+        path = mp.getPath()
+    print("path:")
+    for p in path:
+        print(p)
     print("planning took: ", time.time() - t)
+    # vis.add("path", path)
+    # vis.setColor("path", 0, 1, 0, 1)
 
-    planner.space.close()
-    planner.close()
+    vis.spin(2)
+    for p in path:
+        robot.setConfig(p)
+        vis.spin(1)
 
     vis.spin(int(1e9))
 
@@ -113,3 +119,45 @@ if __name__ == '__main__':
 
     pass
 
+"""
+PLANNING with constraints example from chatgpt:
+
+
+from klampt import robotplanning
+from klampt import ik
+
+# Define your target Cartesian position
+target_position = [x, y, z]  # Replace x, y, z with your desired position coordinates
+
+# Define orientation constraints for four rotation options around Z-axis (0, 90, 180, 270 degrees)
+orientation_constraints = [
+    ik.constraints.RotationConeConstraint(orientation=[0, 0, 1], angle=5),  # Option 1: within 5 degrees of Z-axis
+    ik.constraints.RotationConeConstraint(orientation=[0, 0, 1], angle=5, axis=[0, 0, 1]),  # Option 2: within 5 degrees around Z-axis
+    ik.constraints.RotationConeConstraint(orientation=[0, 0, 1], angle=5, axis=[0, 0, -1]),  # Option 3: within 5 degrees around -Z-axis
+    ik.constraints.RotationConeConstraint(orientation=[0, 0, 1], angle=5, axis=[0, 0, 1]),  # Option 4: within 5 degrees around Z-axis
+]
+
+# Combine orientation constraints using logical "OR" operator
+composite_orientation_constraint = ik.constraints.CustomConstraint()
+composite_orientation_constraint.addTerms(orientation_constraints, operator='OR')
+
+# Create the IK objective with position and composite orientation constraint
+ik_objective = ik.objective.CustomIKObjective()
+ik_objective.setFixedPoint(link, localpos=target_position)
+ik_objective.addConstraint(composite_orientation_constraint)
+
+# Define the list of IK objectives for the motion planner
+ik_objectives = [ik_objective]
+
+try:
+    # Plan motion with IK constraints
+    result = robotplanning.plan_to_cartesian_objective(world, robot, ik_objectives)
+    
+    # Check if planning succeeded
+    if result is not None:
+        print("Motion planning successful.")
+    else:
+        print("Failed to plan motion.")
+except Exception as e:
+    print(f"Motion planning error: {e}")
+"""
