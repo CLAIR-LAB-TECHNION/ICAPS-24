9ee1a0c13a712f74c0bdf6e0b449f89195c4dfe7
yuvalgos <yuvalgos@gmail.com>
2024-05-07 20:56:45 +0300

    reduce base rotation joint limit again

--- motion_planning/configurations.py
+++ motion_planning/configurations.py
@@ -3,8 +3,8 @@ import numpy as np
 # constraint joint limits for faster planning. joint 2 is base rotation, don't need entire 360,
 # joint 3 is shoulder lift, outside the limits will probably result table collision anyway.
 # joint 4 has 2 pi range anyway. all the others can do fine with 3pi range
-limits_l = [0, -2 * np.pi, -4.5, -np.pi, -3 * np.pi / 2, -3 * np.pi / 2, -3 * np.pi / 2, 0]
-limits_h = [0, 2 * np.pi, 1., np.pi, 3 * np.pi / 2, 3 * np.pi / 2, 3 * np.pi / 2, 0]
+limits_l = [0, -1.5 * np.pi, -4.5, -np.pi, -3 * np.pi / 2, -3 * np.pi / 2, -3 * np.pi / 2, 0]
+limits_h = [0, 1.5 * np.pi, 1., np.pi, 3 * np.pi / 2, 3 * np.pi / 2, 3 * np.pi / 2, 0]
 
 default_config = {
     # "type": "lazyrrg*",
